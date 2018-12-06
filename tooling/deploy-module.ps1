3989021e892784f43d2f7ac75192f90df70baba4
Todor Mitskovski <todorbm85@gmail.com>
2018-12-06 09:39:18 +0200

    Fix deployment scripts

--- tooling/deploy-module.ps1
+++ tooling/deploy-module.ps1
@@ -10,7 +10,7 @@ Get-ChildItem $destination |
     Where-Object { -not $filtered.Contains($_.Name) } |
     Remove-Item -Force -Recurse
 
-Get-ChildItem -Path "$PSScriptRoot\module" |
+Get-ChildItem -Path "$PSScriptRoot\..\module" |
     Where-Object { -not $filtered.Contains($_.Name) } |
     Copy-Item -Destination $destination -Recurse
 
--- tooling/new-manifest.ps1
+++ tooling/new-manifest.ps1
@@ -4,9 +4,7 @@ Param(
 
 # module discovery
 $rootModule = "sf-dev"
-$scripts = Get-ChildItem "$PSScriptRoot\..\module" -Recurse | where { $_.Extension -eq '.ps1' -or $_.Extension -eq '.psm1'}
-$nestedmodulesNames = $scripts | where { $_.BaseName -ne $rootModule -and $_.Extension -eq '.psm1'} | % { $_.BaseName }
-$requiredModules = @("toko-domains", "toko-admin")
+$scripts = Get-ChildItem "$PSScriptRoot\..\module\core" -Recurse | where { $_.Extension -eq '.ps1'}
 # functions discovery
 $modulesLines = $scripts | Get-Content
 $functionsLines = $modulesLines | where { $_.contains("function") }
@@ -15,7 +13,6 @@ $functionNamePattern = "^\s*?function\s+?(?<name>(sf-).+?)\s+({|\().*$"
 $filteredNames = $functionsLines | where { $_ -match $functionNamePattern } | % { $Matches["name"] }
 $functionNames = New-Object System.Collections.ArrayList($null)
 $functionNames.AddRange($filteredNames) > $null
-# $functionNames.Add("_get-selectedProject") > $null
 
 if ($env -eq 'dev') {
     $functionNames = '*'
@@ -23,18 +20,13 @@ if ($env -eq 'dev') {
 
 # generate manifest
 New-ModuleManifest `
-    -Path "$PSScriptRoot\..\module\$rootModule.psd1" `
+    -Path "$PSScriptRoot\..\module\sf-dev.psd1" `
     -RootModule $rootModule `
     -ModuleVersion '1.0' `
-    -NestedModules $nestedmodulesNames `
     -FunctionsToExport $functionNames `
     -CmdletsToExport '' `
     -VariablesToExport '' `
     -Author 'Todor Mitskovski' `
     -Description 'Sitefinity core dev automation tools' `
-    -RequiredModules $requiredModules `
+    -ScriptsToProcess 'bootstrap\load-nestedModules.ps1' `
     -PowerShellVersion '5.0' -ClrVersion '4.0';
-
-
-# type in ps console window to generate documentation form comments in functions
-# Get-Command -Module SfTool | % { Get-Help $_ -full; Write-Host "`n----------------------------------------------------`n";} >> export.txt
