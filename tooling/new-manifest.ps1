73acaa7e972b578e0552724fab269a39b0ce3f97
Todor Mitskovski <todorbm85@gmail.com>
2019-02-03 16:00:06 +0200

    Generate developer manifest separately

--- tooling/new-manifest.ps1
+++ tooling/new-manifest.ps1
@@ -1,8 +1,5 @@
-Param(
-    $env
-)
-
 # module discovery
+$filePath = "$PSScriptRoot\..\module\sf-dev.psd1"
 $rootModule = "sf-dev"
 $scripts = Get-ChildItem "$PSScriptRoot\..\module\core" -Recurse | Where-Object { $_.Extension -eq '.ps1'}
 # functions discovery
@@ -14,21 +11,25 @@ $filteredNames = $functionsLines | Where-Object { $_ -match $functionNamePattern
 $functionNames = New-Object System.Collections.ArrayList($null)
 $functionNames.AddRange($filteredNames) > $null
 
-if ($env -eq 'dev') {
-    $functionNames = '*'
+function create-module {
+    # generate manifest
+    New-ModuleManifest `
+        -Path $filePath `
+        -RootModule $rootModule `
+        -ModuleVersion '1.0' `
+        -FunctionsToExport $functionNames `
+        -CmdletsToExport '' `
+        -VariablesToExport '' `
+        -Author 'Todor Mitskovski' `
+        -CompanyName ' ' `
+        -Description 'Sitefinity core dev automation tools' `
+        -ScriptsToProcess 'bootstrap\load-nestedModules.ps1' `
+        -Guid "570fb657-4d88-4883-8b39-2dae4db1280c" `
+        -PowerShellVersion '5.1' -ClrVersion '4.0';
 }
 
-# generate manifest
-New-ModuleManifest `
-    -Path "$PSScriptRoot\..\module\sf-dev.psd1" `
-    -RootModule $rootModule `
-    -ModuleVersion '1.0' `
-    -FunctionsToExport $functionNames `
-    -CmdletsToExport '' `
-    -VariablesToExport '' `
-    -Author 'Todor Mitskovski' `
-    -CompanyName ' ' `
-    -Description 'Sitefinity core dev automation tools' `
-    -ScriptsToProcess 'bootstrap\load-nestedModules.ps1' `
-    -Guid "570fb657-4d88-4883-8b39-2dae4db1280c" `
-    -PowerShellVersion '5.1' -ClrVersion '4.0';
+create-module
+
+$filePath = "$PSScriptRoot\..\module\sf-dev.dev.psd1"
+$functionNames = '*'
+create-module
