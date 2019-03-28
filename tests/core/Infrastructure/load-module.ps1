52948c7897c34ab2f2d665c1419f791fac157a64
todorm85 <todorbm85@gmail.com>
2019-03-28 13:07:59 +0200

    Tests fix load module

--- tests/core/Infrastructure/load-module.ps1
+++ tests/core/Infrastructure/load-module.ps1
@@ -1,1 +1,1 @@
-Import-Module "C:\Users\User\Desktop\sf-dev\module\sf-dev.dev.psd1" -Force -DisableNameChecking -ArgumentList "$PSScriptRoot\config.testing.ps1"
+Import-Module "$PSScriptRoot\..\..\..\module\sf-dev.dev.psd1" -Force -DisableNameChecking -ArgumentList "$PSScriptRoot\config.testing.ps1"
