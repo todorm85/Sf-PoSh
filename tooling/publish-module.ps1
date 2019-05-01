dfc422d2773df8dc8fe840d1051b935e83c411e6
todorm85 <todorbm85@gmail.com>
2019-05-01 10:52:28 +0300

    Update publish script

--- tooling/publish-module.ps1
+++ tooling/publish-module.ps1
@@ -2,7 +2,7 @@ Set-Location "$PSScriptRoot\..\"
 
 $version = Read-Host -Prompt "Version: "
 
-.\new-manifest.ps1 $version
+& "$PSScriptRoot\new-manifest.ps1" $version
 
 git clean -xdf
 
@@ -10,4 +10,4 @@ $apiKey = Read-Host -Prompt "API Key: "
 $notes = Read-Host -Prompt "Release notes: "
 Publish-Module -Name "sf-dev" -NuGetApiKey $apiKey -ReleaseNote $notes -ProjectUri "https://github.com/todorm85/sitefinity-dev-orchestration"
 
-.\new-manifest.ps1 $version
+& "$PSScriptRoot\new-manifest.ps1" $version
