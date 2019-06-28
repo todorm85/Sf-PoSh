a7049b593bb6ebafba0face12f3bef16f20266c9
todorm85 <todorbm85@gmail.com>
2019-06-28 16:02:35 +0300

    Update module publish script

--- tooling/publish-module.ps1
+++ tooling/publish-module.ps1
@@ -1,5 +1,11 @@
+Param(
+    [Parameter(Mandatory = $true)][string]$version
+)
+
 Set-Location "$PSScriptRoot"
 . "./docs-generator.ps1"
 git commit --quiet -a -m "Update docs"
+
+git tag $version
 
 Publish-Module -Name "sf-dev" -NuGetApiKey $Env:NuGetApiKey
