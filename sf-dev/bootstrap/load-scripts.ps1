3cd361d45adca55be2499fabea25a730c6556905
todorm85 <todorbm85@gmail.com>
2019-10-31 18:50:46 +0200

    Load required webadministration module

--- sf-dev/bootstrap/load-scripts.ps1
+++ sf-dev/bootstrap/load-scripts.ps1
@@ -6,6 +6,8 @@ function Load-InitScriptFiles ($path) {
     Get-ChildItem -Path $path -Filter '*.ps1' -Recurse | Where-Object { $_.Name.EndsWith(".init.ps1") }
 }
 
+Import-Module WebAdministration -Force
+
 # Do not dot source in function scope it won`t be loaded inside the module
 Load-InitScriptFiles "$PSScriptRoot\..\core" | ForEach-Object { . $_.FullName }
 Load-InitScriptFiles "$PSScriptRoot\..\admin" | ForEach-Object { . $_.FullName }
