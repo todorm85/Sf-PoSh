b385057ee85aedfee1c9d7e14dc24646615e77c9
todorm85 <todorbm85@gmail.com>
2020-03-02 04:10:57 +1400

    Add easymode aliases

--- sf-dev/sf-dev.psm1
+++ sf-dev/sf-dev.psm1
@@ -9,12 +9,22 @@ if (-not (Test-Path $Script:moduleUserDir)) {
 . "${PSScriptRoot}/bootstrap/init-psPrompt.ps1"
 . "${PSScriptRoot}/bootstrap/load-scripts.ps1"
 
-Set-Alias -Name sf-project-create -Value sd-project-create -Scope global
-Set-Alias -Name sf-project-remove -Value sd-project-remove -Scope global
-Set-Alias -Name sf-project-rename -Value sd-project-rename -Scope global
-Set-Alias -Name sf-project-select -Value sd-project-select -Scope global
-Set-Alias -Name sf-project-showCurrent -Value sd-project-show -Scope global
-Set-Alias -Name sf-project-showAll -Value sd-project-showAll -Scope global
-Set-Alias -Name sf-project-clone -Value sd-project-clone -Scope global
+Set-Alias -Name sf-create -Value sd-project-create -Scope global
+Set-Alias -Name sf-remove -Value sd-project-remove -Scope global
+Set-Alias -Name sf-rename -Value sd-project-rename -Scope global
+Set-Alias -Name sf-select -Value sd-project-select -Scope global
+Set-Alias -Name sf-show -Value sd-project-show -Scope global
+Set-Alias -Name sf-clone -Value sd-project-clone -Scope global
+Set-Alias -Name sf-reinitialize -Value sd-app-reinitializeAndStart -Scope global
+Set-Alias -Name sf-restart -Value sd-iisAppPool-Reset -Scope global
+Set-Alias -Name sf-precompiledTemplates-add -Value sd-appPrecompiledTemplates-add -Scope global
+Set-Alias -Name sf-precompiledTemplates-remove -Value sd-appPrecompiledTemplates-remove -Scope global
+Set-Alias -Name sf-rebuild -Value sd-sol-rebuild -Scope global
+Set-Alias -Name sf-build -Value sd-sol-build -Scope global
+Set-Alias -Name sf-getLatest -Value sd-sourceControl-getLatestChanges -Scope global
+Set-Alias -Name sf-save -Value sd-appStates-save -Scope global
+Set-Alias -Name sf-restore -Value sd-appStates-restore -Scope global
+Set-Alias -Name sf-subApp-set -Value sd-iisSubApp-set -Scope global
+Set-Alias -Name sf-subApp-remove -Value sd-iisSubApp-remove -Scope global
 
 Export-ModuleMember -Function * -Alias *
