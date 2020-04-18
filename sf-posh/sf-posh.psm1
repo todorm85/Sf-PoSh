c4e8f3dbdb42414ea8dbbc2fc96e0b3085bf1bc5
Todor Mitskovski <todorbm85@gmail.com>
2020-04-18 18:36:01 +0300

    Reformat

--- sf-posh/sf-posh.psm1
+++ sf-posh/sf-posh.psm1
@@ -5,10 +5,10 @@ if (-not (Test-Path $Script:moduleUserDir)) {
     New-Item -Path $Script:moduleUserDir -ItemType Directory
 }
 
-. "$PSScriptRoot/bootstrap/init-config.ps1"
-. "$PSScriptRoot/bootstrap/initialize-events.ps1"
-. "$PSScriptRoot/bootstrap/init-psPrompt.ps1"
-. "$PSScriptRoot/bootstrap/load-scripts.ps1"
-. "$PSScriptRoot/bootstrap/run-upgrades.ps1"
+. "$PSScriptRoot\bootstrap\init-config.ps1"
+. "$PSScriptRoot\bootstrap\initialize-events.ps1"
+. "$PSScriptRoot\bootstrap\init-psPrompt.ps1"
+. "$PSScriptRoot\bootstrap\load-scripts.ps1"
+. "$PSScriptRoot\bootstrap\run-upgrades.ps1"
 
 Export-ModuleMember -Function *
