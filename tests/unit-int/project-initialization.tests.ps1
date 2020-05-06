5f9258523eef990746b75c5b145f642e2df1f094
Todor Mitskovski <todorbm85@gmail.com>
2020-05-07 00:22:23 +0300

    Formatting

--- tests/unit-int/project-initialization.tests.ps1
+++ tests/unit-int/project-initialization.tests.ps1
@@ -5,36 +5,36 @@ InModuleScope sf-posh {
     Describe "Project initialization should" {
         InTestProjectScope {
 
-        [SfProject]$p = sf-project-get
-        $oldWebsiteName = $p.websiteName
-        $oldSolPath = $p.solutionPath
-        It "not initialize when using the api" {
-            $p.websiteName | Should -Not -BeNullOrEmpty
-            $p.websiteName = 'wrongName'
-            $p.solutionPath = 'dummyPath'
-            sf-project-save $p
             [SfProject]$p = sf-project-get
-            $p.websiteName | Should -Be 'wrongName'
-            $p.solutionPath | Should -Be 'dummyPath'
-            [SfProject]$p = sf-project-get -all | select -First 1
-            $p.websiteName | Should -Be 'wrongName'
-            $p.solutionPath | Should -Be 'dummyPath'
-        }
-
-        It "initialize when using select from the prompt and not in cache" {
-            [SfProject]$p = sf-project-get -all | select -First 1
-            $p.websiteName = 'wrongName2'
-            $p.solutionPath = 'dummyPath2'
-
-            Mock _proj-promptSelect {
-                $p
+            $oldWebsiteName = $p.websiteName
+            $oldSolPath = $p.solutionPath
+            It "not initialize when using the api" {
+                $p.websiteName | Should -Not -BeNullOrEmpty
+                $p.websiteName = 'wrongName'
+                $p.solutionPath = 'dummyPath'
+                sf-project-save $p
+                [SfProject]$p = sf-project-get
+                $p.websiteName | Should -Be 'wrongName'
+                $p.solutionPath | Should -Be 'dummyPath'
+                [SfProject]$p = sf-project-get -all | select -First 1
+                $p.websiteName | Should -Be 'wrongName'
+                $p.solutionPath | Should -Be 'dummyPath'
             }
 
-            sf-project-select
-            [SfProject]$p = sf-project-get
-            $p.websiteName | Should -Be $oldWebsiteName
-            $p.solutionPath | Should -Be $oldSolPath
-        }
+            It "initialize when using select from the prompt and not in cache" {
+                [SfProject]$p = sf-project-get -all | select -First 1
+                $p.websiteName = 'wrongName2'
+                $p.solutionPath = 'dummyPath2'
+
+                Mock _proj-promptSelect {
+                    $p
+                }
+
+                sf-project-select
+                [SfProject]$p = sf-project-get
+                $p.websiteName | Should -Be $oldWebsiteName
+                $p.solutionPath | Should -Be $oldSolPath
+            }
 
         }
     }
