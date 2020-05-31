89b78c6e6ac2bb3f92208b5e2f954c3c73aee020
Todor Mitskovski <todorbm85@gmail.com>
2020-05-31 19:25:28 +0300

    Update for publishing

--- sf-posh/sf-posh.psm1
+++ sf-posh/sf-posh.psm1
@@ -12,15 +12,3 @@ if (-not (Test-Path $Script:moduleUserDir)) {
 . "$PSScriptRoot\bootstrap\run-upgrades.ps1"
 
 Export-ModuleMember -Function *
-
-$current = _getLoadedModuleVersion
-$updatesPath = "\\tmitskov\sf-posh"
-$latestVersion = Get-ChildItem -Path $updatesPath -Directory | Sort-Object -Property CreationTime -Descending | Select -First 1
-if ($latestVersion -and (_isFirstVersionLower $current $latestVersion.name)) {
-    $source = $latestVersion.FullName
-    unlock-allFiles -path $PSScriptRoot
-    Remove-Item "$PSScriptRoot\*" -Force -Recurse
-    Copy-Item "$source\*" $PSScriptRoot -Force -Recurse
-    Write-Warning "Module updated to latest version. Reloading..."
-    Import-Module "$PSScriptRoot\sf-posh.psd1" -Force
-}
