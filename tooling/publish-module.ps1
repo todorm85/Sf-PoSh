565927cdbb3df6bcddaad279f0a2f26a9dc61f10
Todor Mitskovski <todorbm85@gmail.com>
2020-05-31 19:08:50 +0300

    Update for publishing

--- tooling/publish-module.ps1
+++ tooling/publish-module.ps1
@@ -29,5 +29,5 @@ if ($res -and $res.ToString().Contains('fatal')) {
 # Copy-SfToLive
 $version = _getLoadedModuleVersion
 $destination = "$PSScriptRoot\..\..\dist\$version"
-New-Item $destination -Force
+New-Item $destination -Force -ItemType Directory
 Copy-Item "$PSScriptRoot\..\sf-posh\*" $destination -Force -Recurse
