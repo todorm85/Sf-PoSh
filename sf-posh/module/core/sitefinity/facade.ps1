c7a50ec174d53a1877772fca52ed6a490be83859
Todor Mitskovski <todorbm85@gmail.com>
2020-11-18 16:40:12 +0200

    Simplify facade

--- sf-posh/module/core/sitefinity/facade.ps1
+++ sf-posh/module/core/sitefinity/facade.ps1
@@ -1,62 +1,39 @@
 function sf {
-    [CmdletBinding()]
     param (
-        [Parameter(ParameterSetName = "recreate")][switch]$recreate,
-        [Parameter(ParameterSetName = "startNew")][switch]$startNew,
-        [Parameter(ParameterSetName = "sync")][switch]$sync,
-        [Parameter(ParameterSetName = "generic")][switch]$getLatest,
-        [Parameter(ParameterSetName = "generic")][switch]$forceGet,
-        [Parameter(ParameterSetName = "generic")][switch]$discardExisting,
-        [Parameter(ParameterSetName = "generic")][switch]$abortNoNew,
-        [Parameter(ParameterSetName = "generic")][switch]$clean,
+        [Parameter(ParameterSetName = "generic")][switch]$getLatestChanges,
+        [Parameter(ParameterSetName = "generic")][switch]$forceGetChanges,
+        [Parameter(ParameterSetName = "generic")][switch]$discardExistingChanges,
+        [Parameter(ParameterSetName = "generic")][switch]$stopWhenNoNewChanges,
+        [Parameter(ParameterSetName = "generic")][switch]$cleanSolution,
         [Parameter(ParameterSetName = "generic")][switch]$build,
-        [Parameter(ParameterSetName = "generic")][switch]$reinitialize,
-        [Parameter(ParameterSetName = "reset")][switch]$reset,
+        [Parameter(ParameterSetName = "generic")][switch]$resetApp,
+        [Parameter(ParameterSetName = "generic")][switch]$resetPool,
         [Parameter(ParameterSetName = "generic")][switch]$precompile,
-        [Parameter(ParameterSetName = "start")][switch]$start,
-        [Parameter(ParameterSetName = "generic")][switch]$save,
+        [Parameter(ParameterSetName = "generic")][switch]$ensureRunning,
+        [Parameter(ParameterSetName = "generic")][switch]$saveInitialState,
         [Parameter(ValueFromPipeline)]
         [SfProject]$project
     )
     
     Process {
         Run-InFunctionAcceptingProjectFromPipeline {
-            if ($sync) {
-                sf-appPrecompiledTemplates-remove
-                sf -getLatest -abortNoNew -build
-                sf-app-sendRequestAndEnsureInitialized
-                return
-            }
-
-            if ($recreate) {
-                sf -getLatest -forceGet -discardExisting -clean -build -reinitialize -precompile -save
-                return
-            }
-
-            if ($startNew) {
-                sf-appPrecompiledTemplates-remove
-                sf -getLatest -discardExisting -abortNoNew -build
-                sf-app-sendRequestAndEnsureInitialized
-                return
-            }
-
-            $newChangesDetected = $forceGet # force will always get new changes
-            if ($discardExisting -and (sf-sourceControl-hasPendingChanges)) {
+            $newChangesDetected = $forceGetChanges # force will always get new changes
+            if ($discardExistingChanges -and (sf-sourceControl-hasPendingChanges)) {
                 $output = sf-sourceControl-undoPendingChanges
                 $newChangesDetected = $output.Exception -and !($output.Exception -notlike "*No pending changes*")
             }
         
-            if ($getLatest) {
+            if ($getLatestChanges) {
                 $getLatestOutput = sf-sourceControl-getLatestChanges -overwrite:$forceGet
                 $newChangesDetected = !$getLatestOutput -or !($getLatestOutput.Contains('All files are up to date.'))
             }
         
-            if (!$newChangesDetected -and $abortNoNew) {
+            if (!$newChangesDetected -and $stopWhenNoNewChanges) {
                 Write-Information "No new changes detected, stopping."
                 return
             }
 
-            if ($clean) {
+            if ($cleanSolution) {
                 sf-sol-clean -cleanPackages $true
             }
 
@@ -64,11 +41,11 @@ function sf {
                 sf-sol-build -retryCount 3
             }
 
-            if ($reset) {
+            if ($resetPool) {
                 sf-iisAppPool-Reset
             }
 
-            if ($reinitialize) {
+            if ($resetApp) {
                 sf-app-reinitialize
             }
 
@@ -77,11 +54,11 @@ function sf {
                 sf-app-sendRequestAndEnsureInitialized
             }
 
-            if ($start) {
+            if ($ensureRunning) {
                 sf-app-sendRequestAndEnsureInitialized
             }
 
-            if ($save) {
+            if ($saveInitialState) {
                 Start-Sleep -Seconds 1
                 sf-appStates-save -stateName "init"
             }
